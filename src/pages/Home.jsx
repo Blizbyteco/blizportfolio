4c648512db3b71cdf374b3195e1ca75aee64824d
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-09-06 17:11:10 +0700

    ADD: Description to banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,12 +26,18 @@ export default function Home() {
       <div className="absolute left-0 top-0 size-full bg-black bg-opacity-75 text-6xl text-white flex items-end"></div>
       {/* end backdrop */}
 
+      <div className="absolute right-8 top-[20%] z-10 text-white">
+        <p className="text-sm md:text-base max-w-sm md:max-w-lg text-right">
+        Our company creates high-performance web and Android apps, delivering reliable UI/UX designs and tailored solutions using the latest technology.
+        </p>
+      </div>
+
       <div className="absolute bottom-[20%] left-8 z-10">
         <motion.div
           initial="initial"
           animate="animate"
           transition={{ staggerChildren: 0.03, ease: easeIn }}
-          className=" font-primary font-medium text-white text-4xl md:text-6xl overflow-hidden flex py-1  items-center"
+          className=" font-primary font-medium text-white text-4xl md:text-6xl overflow-hidden flex py-1 items-center"
         >
           {"Turn your dream app".split("").map((word, index) => (
             <motion.h2 variants={textAnimations} key={index}>{word == " " ? "\u00A0" : word}</motion.h2>
@@ -41,7 +47,7 @@ export default function Home() {
           initial="initial"
           animate="animate"
           transition={{ staggerChildren: 0.03, ease: easeIn, delayChildren: 0 }}
-          className="font-primary font-medium text-white text-4xl md:text-6xl overflow-hidden flex mt-4 py-1 items-center"
+          className="font-primary font-medium text-white text-4xl md:text-6xl overflow-hidden flex mt-1 md:mt-4 py-1 items-center"
         >
          {"into reality".split("").map((word, index) => (
             <motion.h2 variants={textAnimations} key={index}>{word == " " ? "\u00A0" : word}</motion.h2>
