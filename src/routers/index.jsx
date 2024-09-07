d8ed5c19a3a4d254badf60b366985409ee18a0f9
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-09-07 17:12:01 +0700

    UPDATE: Changed BrowserRouter to HashRouter

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import Company from '../pages/Company'
 import RootLayout from '../layouts/RootLayout'
 
 export default function Router() {
   return (
-    <BrowserRouter>
+    <HashRouter>
         <Routes>
             <Route path="/" element={<RootLayout />}>
                 <Route path="/" element={<Home />}  />
                 <Route path="company/" element={<Company />} />
             </Route>
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
