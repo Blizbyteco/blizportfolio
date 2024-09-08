8c7ff28811885ab39244ff95e532c9bd943bbcf9
frmnxhken <frmnxhken@gmail.com>
2024-09-08 21:10:14 +0700

    Modify card team

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -1,41 +1,47 @@
-import { motion } from 'framer-motion'
 import React from 'react'
+import { motion } from 'framer-motion'
 
-export default function Teams({y}) {
-  return (
-    <motion.div 
-    className='px-12 py-36'
-    style={{
-        y: y
-    }}
-    >
-        <h1 className='text-6xl font-semibold'>Meet our experts</h1>
-        <div className='grid grid-cols-3 w-full py-20 px-12'>
-            {/* cards */}
-            <div className=' w-[350px] h-[400px] bg-[url("http://localhost:5173/profile.jpg")] bg-center bg-cover grayscale group'>
-                <div className='group-hover:opacity-75 opacity-0 bg-black w-full h-full absolute top-0 left-0 text-white flex flex-col justify-center items-center transition-opacity duration-150 ease-in'>
-                    <h3 className='text-2xl font-semibold'>Alvin Setya Pranata</h3>
-                    <p>Backend Developer</p>
-                </div>
+export default function Teams({ y }) {
+    const BASE_URL = window.location.origin;
+    const data = [
+        {
+            name: "Alvin Setya Pranata",
+            role: "Backend Developer",
+            photo: "alvin.jpg"
+        },
+        {
+            name: "Wahyudi Firmansyah",
+            role: "Frontend Developer",
+            photo: "wahyudi.jpg"
+        },
+        {
+            name: "Syaiful Miqdar",
+            role: "UI/UX Designer",
+            photo: "ipul.png" 
+        }
+    ];
+    return (
+        <motion.div
+            className='px-12 py-36'
+            style={{
+                y: y
+            }}
+        >
+            <h1 className='text-6xl font-semibold'>Meet our experts</h1>
+
+            <div className='grid grid-cols-3 w-full py-20 px-12 gap-x-12'>
+                {/* cards */}
+                {data.map((profile, index) => (
+                    <div className="relative">
+                        <img className='w-full h-full aspect-square object-cover grayscale' src={BASE_URL + "/photos/" + profile.photo} alt="profile" />
+                        <div className='opacity-0 hover:opacity-70 bg-black w-full h-full absolute top-0 left-0 text-white flex flex-col justify-center items-center transition-opacity duration-150 ease-in'>
+                            <h3 className='text-2xl font-semibold'>{profile.name} </h3>
+                            <p>{profile.role}</p>
+                        </div>
+                    </div>
+                ))}
+                {/* end cards */}
             </div>
-            {/* end cards */}
-            {/* cards */}
-            <div className=' w-[350px] h-[400px] bg-[url("http://localhost:5173/firman.webp")] bg-center bg-cover grayscale group'>
-                <div className='group-hover:opacity-75 opacity-0 bg-black w-full h-full absolute top-0 left-0 text-white flex flex-col justify-center items-center transition-opacity duration-150 ease-in'>
-                    <h3 className='text-2xl font-semibold'>Wahyudi Firmansyah</h3>
-                    <p>Frontend Developer</p>
-                </div>
-            </div>
-            {/* end cards */}
-            {/* cards */}
-            <div className=' w-[350px] h-[400px] bg-[url("http://localhost:5173/ipul.png")] bg-center bg-cover grayscale group'>
-                <div className='group-hover:opacity-75 opacity-0 bg-black w-full h-full absolute top-0 left-0 text-white flex flex-col justify-center items-center transition-opacity duration-150 ease-in'>
-                    <h3 className='text-2xl font-semibold'>Syaiful Miqdar</h3>
-                    <p>UI/UX Designer</p>
-                </div>
-            </div>
-            {/* end cards */}
-        </div>
-    </motion.div>
-  )
+        </motion.div>
+    )
 }
