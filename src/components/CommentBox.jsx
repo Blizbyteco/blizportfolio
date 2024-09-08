985daf5f62db372e75367d8e15cce214f8dce520
frmnxhken <frmnxhken@gmail.com>
2024-09-08 20:43:20 +0700

    Add slider and change commentbox style

--- src/components/CommentBox.jsx
+++ src/components/CommentBox.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-export default function CommentBox({name, role, content}) {
+export default function CommentBox({name, role, content, company}) {
   return (
-    <div className='shrink-0 bg-white rounded-md px-6 py-8 max-w-xl shadow-md'>
-        <div className='flex justify-between items-center'>
+    <div className='shrink-0 bg-white rounded-md px-6 py-8 max-w-xl border border-slate-200'>
+        <div className='flex justify-between items-end'>
             <div>
                 <h4 className='font-semibold text-lg'>{name}</h4>
                 <span className='text-sm text-gray-500'>{role}</span>
             </div>
-            <p className='text-base'>Maduraholic</p>
+            <p className='text-sm'>{company}</p>
         </div>
 
         <p className='mt-6'>
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,32 +1,64 @@
 import React from "react";
 import CommentBox from "./CommentBox";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import { Pagination } from 'swiper/modules';
 
 export default function Comments() {
+  const data = [
+    {
+      name: "Ahmad Zulhan Bakri",
+      role: "Founder & CEO",
+      company: "Maduraholic",
+      content: "Saya sangat puas dengan hasil kerja dari tim Blizbyteco. Mereka telah membuat website yang tidak hanya tampak profesional, tetapi juga sangat user-friendly dan sesuai dengan kebutuhan usaha saya. Proses komunikasi berjalan lancar, dan setiap masukan yang saya berikan direspons dengan cepat dan akurat. Terima kasih untuk dedikasi dan kualitas kerja yang luar biasa! Saya tidak ragu merekomendasikan layanan Blizbyteco kepada siapa pun yang membutuhkan solusi website terbaik."
+    },
+    {
+      name: "Ahmad Zulhan Bakri",
+      role: "Founder & CEO",
+      company: "Maduraholic",
+      content: "Saya sangat puas dengan hasil kerja dari tim Blizbyteco. Mereka telah membuat website yang tidak hanya tampak profesional, tetapi juga sangat user-friendly dan sesuai dengan kebutuhan usaha saya. Proses komunikasi berjalan lancar, dan setiap masukan yang saya berikan direspons dengan cepat dan akurat. Terima kasih untuk dedikasi dan kualitas kerja yang luar biasa! Saya tidak ragu merekomendasikan layanan Blizbyteco kepada siapa pun yang membutuhkan solusi website terbaik."
+    },
+    {
+      name: "Ahmad Zulhan Bakri",
+      role: "Founder & CEO",
+      company: "Maduraholic",
+      content: "Saya sangat puas dengan hasil kerja dari tim Blizbyteco. Mereka telah membuat website yang tidak hanya tampak profesional, tetapi juga sangat user-friendly dan sesuai dengan kebutuhan usaha saya. Proses komunikasi berjalan lancar, dan setiap masukan yang saya berikan direspons dengan cepat dan akurat. Terima kasih untuk dedikasi dan kualitas kerja yang luar biasa! Saya tidak ragu merekomendasikan layanan Blizbyteco kepada siapa pun yang membutuhkan solusi website terbaik."
+    },
+    {
+      name: "Ahmad Zulhan Bakri",
+      role: "Founder & CEO",
+      company: "Maduraholic",
+      content: "Saya sangat puas dengan hasil kerja dari tim Blizbyteco. Mereka telah membuat website yang tidak hanya tampak profesional, tetapi juga sangat user-friendly dan sesuai dengan kebutuhan usaha saya. Proses komunikasi berjalan lancar, dan setiap masukan yang saya berikan direspons dengan cepat dan akurat. Terima kasih untuk dedikasi dan kualitas kerja yang luar biasa! Saya tidak ragu merekomendasikan layanan Blizbyteco kepada siapa pun yang membutuhkan solusi website terbaik."
+    },
+  ];
   return (
     <div className="py-36 px-12">
       <h2 className="text-6xl font-semibold">What they said</h2>
-
-      <div className="flex gap-x-24 py-24 overflow-auto">
-        <CommentBox
-            name="Ahmad Zulhan Bakri"
-            role="Founder & CEO"
-            content="Saya sangat puas dengan hasil kerja dari tim Blizbyteco. 
-    Mereka telah membuat website yang tidak hanya tampak profesional, tetapi juga sangat user-friendly dan sesuai dengan kebutuhan usaha saya. Proses komunikasi berjalan lancar, dan setiap masukan yang saya berikan direspons dengan cepat dan akurat. Terima kasih untuk dedikasi dan kualitas kerja yang luar biasa! Saya tidak ragu merekomendasikan layanan Blizbyteco kepada siapa pun yang membutuhkan solusi website terbaik."
-        />
-        <CommentBox
-            name="Ahmad Zulhan Bakri"
-            role="Founder & CEO"
-            content="Saya sangat puas dengan hasil kerja dari tim Blizbyteco. 
-    Mereka telah membuat website yang tidak hanya tampak profesional, tetapi juga sangat user-friendly dan sesuai dengan kebutuhan usaha saya. Proses komunikasi berjalan lancar, dan setiap masukan yang saya berikan direspons dengan cepat dan akurat. Terima kasih untuk dedikasi dan kualitas kerja yang luar biasa! Saya tidak ragu merekomendasikan layanan Blizbyteco kepada siapa pun yang membutuhkan solusi website terbaik."
-        />
-        <CommentBox
-            name="Ahmad Zulhan Bakri"
-            role="Founder & CEO"
-            content="Saya sangat puas dengan hasil kerja dari tim Blizbyteco. 
-    Mereka telah membuat website yang tidak hanya tampak profesional, tetapi juga sangat user-friendly dan sesuai dengan kebutuhan usaha saya. Proses komunikasi berjalan lancar, dan setiap masukan yang saya berikan direspons dengan cepat dan akurat. Terima kasih untuk dedikasi dan kualitas kerja yang luar biasa! Saya tidak ragu merekomendasikan layanan Blizbyteco kepada siapa pun yang membutuhkan solusi website terbaik."
-        />
+      <div className="py-12">
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          loop={true}
+          pagination={{
+            dynamicBullets: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper py-12"
+        >
+          {data.map((comment, index) => (
+            <SwiperSlide key={index}>
+              <CommentBox
+                name={comment.name}
+                role={comment.role}
+                company={comment.company}
+                content={comment.content}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
-
     </div>
   );
 }
